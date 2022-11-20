275e412d87886283fef383782ade807ac2cbd33d
Jakub Perżyło <qizot1405@gmail.com>
2022-11-20 13:52:50 +0100

    Task 2

--- Models.py
+++ Models.py
@@ -33,3 +33,24 @@ class RibbyOdeModel(Model):
     def compute(self, initial_parameters, time_series):
         result = integrate.odeint(self.__ode_equations, initial_parameters, time_series, args=self.params)
         return result.T
+
+class RibbySimplifiedOdeModel(Model):
+    def __init__(self, params):
+        self.params = params
+
+        def ode_equations(y, t, KDE, lambda_P, K, _1, k_PQ, gamma_P, _2, _3):
+            P, C = y
+            P_star = P
+            # Four differential equations
+            dCdt = -KDE * C
+            dPdt = lambda_P * P * (1 - P_star / K)  - gamma_P * C * KDE * P
+            return dPdt, dCdt
+
+        self.__ode_equations = ode_equations
+
+    def train(self, input_data):
+        pass
+
+    def compute(self, initial_parameters, time_series):
+        result = integrate.odeint(self.__ode_equations, initial_parameters, time_series, args=self.params)
+        return result.T
---
+++ therapy.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def gliomas_therapy(ribby_model, time_range, y0, consts):
+    y = y0
+    t = time_range
+    treatment_results = [[], [], [], []]
+    for i in range(5):
+        P, Q, Qp, C = ribby_model.compute(y, t)
+        next_treatment_month = [ind for ind, x in enumerate(Q) if ind + 1 < Q.size and x < Q[ind + 1]][0]
+        for idx, value in enumerate([P, Q, Qp, C]):
+            treatment_results[idx].append(value[0: next_treatment_month])
+        y = P[next_treatment_month], Q[next_treatment_month], Qp[next_treatment_month], 1
+    treatment_results = [np.concatenate(x) for x in treatment_results]
+    return treatment_results
+
+def gliomas_simple_therapy(ribby_simple_model, time_range, y0, consts):
+    y = y0
+    t = time_range
+    treatment_results = [[], []]
+    for i in range(5):
+        P, C = ribby_simple_model.compute(y, t)
+        next_treatment_month = [ind for ind, x in enumerate(P) if ind + 1 < P.size and x < P[ind + 1]][0]
+        for idx, value in enumerate([P, C]):
+            treatment_results[idx].append(value[0: next_treatment_month])
+        y = P[next_treatment_month], 1
+
+    treatment_results = [np.concatenate(x) for x in treatment_results]
+    return treatment_results
+
